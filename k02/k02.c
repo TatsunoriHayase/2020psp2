9c24e7d3c7b193f68f51cd7b4b6e45863b032f68
TatsunoriHayase <TatsunoriHayase@users.noreply.github.com>
2020-07-20 19:14:18 +0900

    0720_2

--- k02/k02.c
+++ k02/k02.c
@@ -4,14 +4,22 @@
 #include <math.h>
 
 extern double p_stdnorm(double z);
-
+#define MU_A 170.8
+#define MU_B 169.9
+#define sigma_A 5.43
+#define sigma_B 5.5
 int main(void)
 {
-    double val;
+    double val,m1,m2,s1,s2,y1,y2,x1,x2;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
     double L1=1,L2=1;
+
+    m1=MU_A;
+    m2=MU_B;
+    s1=sigma_A;
+    s2=sigma_B;
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -26,12 +34,12 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
-
-
-
+    y1=(val-m1)/s1;
+    y2=(val-m2)/s2;
+    x1=p_stdnorm(y1);
+    x2=p_stdnorm(y2);
+    L1=L1*x1;
+    L2=L2*x2;
     }
 
     if(fclose(fp) == EOF){
@@ -39,8 +47,8 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    printf("L_A: %f\n",max_val);
-    printf("L_B: %f\n",min_val);
+    printf("L_A: %f\n",L1);
+    printf("L_B: %f\n",L2);
 
     return 0;
 
@@ -48,7 +56,7 @@ int main(void)
 }
 
 double p_stdnorm(double z)
-{
+{   double M_PI=3.14;
     return 1/sqrt(2*M_PI) * exp(-z*z/2);
 }
 
