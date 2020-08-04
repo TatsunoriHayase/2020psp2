299b92b9803f2b451aada5a10074ff4282a09306
Kotaro Sonoda <sonoda@cis.nagasaki-u.ac.jp>
2020-08-04 18:15:00 +0900

    課題2の完了を確認しました（薗田）

--- k02/k02.c
+++ k02/k02.c
@@ -10,53 +10,53 @@ extern double p_stdnorm(double z);
 #define sigma_B 5.5
 int main(void)
 {
-    double val,m1,m2,s1,s2,y1,y2,x1,x2;
+    double val, m1, m2, s1, s2, y1, y2, x1, x2;
     char fname[FILENAME_MAX];
     char buf[256];
-    FILE* fp;
-    double L1=1,L2=1;
+    FILE *fp;
+    double L1 = 1, L2 = 1;
 
-    m1=MU_A;
-    m2=MU_B;
-    s1=sigma_A;
-    s2=sigma_B;
+    m1 = MU_A;
+    m2 = MU_B;
+    s1 = sigma_A;
+    s2 = sigma_B;
 
     printf("input the filename of sample:");
-    fgets(fname,sizeof(fname),stdin);
-    fname[strlen(fname)-1] = '\0';
-    printf("the filename of sample: %s\n",fname);
+    fgets(fname, sizeof(fname), stdin);
+    fname[strlen(fname) - 1] = '\0';
+    printf("the filename of sample: %s\n", fname);
 
-    fp = fopen(fname,"r");
-    if(fp==NULL){
-        fputs("File open error\n",stderr);
+    fp = fopen(fname, "r");
+    if (fp == NULL)
+    {
+        fputs("File open error\n", stderr);
         exit(EXIT_FAILURE);
     }
 
-    while(fgets(buf,sizeof(buf),fp) != NULL){
-        sscanf(buf,"%lf",&val);
-    y1=(val-m1)/s1;
-    y2=(val-m2)/s2;
-    x1=p_stdnorm(y1);
-    x2=p_stdnorm(y2);
-    L1=L1*x1;
-    L2=L2*x2;
+    while (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        sscanf(buf, "%lf", &val);
+        y1 = (val - m1) / s1;
+        y2 = (val - m2) / s2;
+        x1 = p_stdnorm(y1);
+        x2 = p_stdnorm(y2);
+        L1 = L1 * x1;
+        L2 = L2 * x2;
     }
 
-    if(fclose(fp) == EOF){
-        fputs("file close error\n",stderr);
+    if (fclose(fp) == EOF)
+    {
+        fputs("file close error\n", stderr);
         exit(EXIT_FAILURE);
     }
 
-    printf("L_A: %f\n",L1);
-    printf("L_B: %f\n",L2);
+    printf("L_A: %f\n", L1);
+    printf("L_B: %f\n", L2);
 
     return 0;
-
-
 }
 
 double p_stdnorm(double z)
-{   
-    return 1/sqrt(2*M_PI) * exp(-z*z/2);
+{
+    return 1 / sqrt(2 * M_PI) * exp(-z * z / 2);
 }
-
