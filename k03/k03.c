bb8ab695d72c67c494197332d28a66301cc0610d
Kotaro Sonoda <sonoda@cis.nagasaki-u.ac.jp>
2020-08-04 18:16:53 +0900

    課題3の完了を確認しました（薗田）

--- k03/k03.c
+++ k03/k03.c
@@ -7,13 +7,13 @@
 #define RAND_SEED 2020
 #else
 #include <time.h>
-#define RAND_SEED ((unsigned) time(NULL))
+#define RAND_SEED ((unsigned)time(NULL))
 #endif
 
 extern double r_unif(void);
 extern double r_stdnorm(void);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     int num_dummy;
     double mu;
@@ -21,24 +21,21 @@ int main(int argc, char* argv[])
     int i;
     double dummy;
 
-
-
-    
-
     printf("============================================\n");
-    sscanf(argv[1],"%lf",&mu);
-    printf("template mean: %4.1lf\n",mu);
-    sscanf(argv[2],"%lf",&sigma);
-    printf("template standard deviation: %3.2lf\n",sigma);
-    sscanf(argv[3],"%d",&num_dummy);
-    printf("Num of dummy data: %d\n",num_dummy);
+    sscanf(argv[1], "%lf", &mu);
+    printf("template mean: %4.1lf\n", mu);
+    sscanf(argv[2], "%lf", &sigma);
+    printf("template standard deviation: %3.2lf\n", sigma);
+    sscanf(argv[3], "%d", &num_dummy);
+    printf("Num of dummy data: %d\n", num_dummy);
     printf("============================================\n");
 
     srand(RAND_SEED);
-    for( i=1;i<=num_dummy;i++ ){
+    for (i = 1; i <= num_dummy; i++)
+    {
         /* r_stdnormを使って，1人のデータを捏造 */
-        dummy=r_stdnorm()*sigma+mu  ;
-        printf("%5.2lf\n",dummy);
+        dummy = r_stdnorm() * sigma + mu;
+        printf("%5.2lf\n", dummy);
     }
 
     return EXIT_SUCCESS;
@@ -46,10 +43,10 @@ int main(int argc, char* argv[])
 
 double r_unif(void)
 {
-    return (double)(rand()+1)/(RAND_MAX+2);
+    return (double)(rand() + 1) / (RAND_MAX + 2);
 }
 
 double r_stdnorm(void)
-{   
-    return sqrt( -2.0*log(r_unif()) ) * sin( 2.0*M_PI*r_unif() );
+{
+    return sqrt(-2.0 * log(r_unif())) * sin(2.0 * M_PI * r_unif());
 }
